372b9b0fe0414817458cd6fd3772988839a06c56
Cory House <housecor@gmail.com>
2023-08-13 16:24:19 -0500

    add comment

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 export default async function ProductsPage() {
   const resp = await fetch("http://localhost:3000/api/products", {
+    // Alternatively, can set via https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#revalidate
     next: { revalidate: 60 }, // Revalidate every 60 seconds
   });
   const products = (await resp.json()) as Product[];
