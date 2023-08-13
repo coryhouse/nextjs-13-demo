eecdb45b7b8a93edea68b2d667d03ddfe1ced25b
Cory House <housecor@gmail.com>
2023-08-13 16:50:27 -0500

    Fix resp

--- app/api/products/[productId]/route.ts
+++ app/api/products/[productId]/route.ts
@@ -1,5 +1,5 @@
 import { mockProducts } from "@/mock-data/products";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 // NOTE: There cannot be a route.js file at the same route segment level as page.js. That's why I put API routes are under /api.
 // GET calls are cached by default.
@@ -8,10 +8,15 @@ export async function GET(
   request: Request,
   { params }: { params: { productId: string } }
 ) {
-  if (!params.productId) return NextResponse.next();
+  if (!params.productId)
+    return NextResponse.next({ status: 400, statusText: "Missing productId." });
+
   const matchingProduct = mockProducts.find(
     (p) => p.id.toString() === params.productId
   );
-  if (!matchingProduct) return NextResponse.next();
+
+  if (!matchingProduct)
+    return NextResponse.next({ status: 404, statusText: "Product not found." });
+
   return NextResponse.json(matchingProduct);
 }
