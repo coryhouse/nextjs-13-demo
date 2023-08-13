7a4ee6d30601fdc6e4d6e41de4968795c841202f
Cory House <housecor@gmail.com>
2023-08-13 18:00:36 -0500

    remove unused

--- utils/cart-utils.ts
+++
@@ -1,9 +0,0 @@
-const cartKey = "cart";
-
-export function addToCart(productId: string) {
-  localStorage.setItem(cartKey, productId);
-}
-
-export function emptyCart() {
-  localStorage.removeItem(cartKey);
-}
