591a11d9581020731483b5dacff60c03f547775b
Cory House <housecor@gmail.com>
2023-08-13 18:34:30 -0500

    add

--- hooks/react-query/useProducts.ts
+++ hooks/react-query/useProducts.ts
@@ -3,10 +3,20 @@ import { useQuery } from "@tanstack/react-query";
 
 export function useProduct(id: string) {
   return useQuery({
-    queryKey: ["product", id],
+    queryKey: ["products", id],
     queryFn: async () => {
       const res = await fetch(`http://localhost:3000/api/products/${id}`);
       return res.json() as Promise<Product>;
+    },
+  });
+}
+
+export function useProducts() {
+  return useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:3000/api/products");
+      return res.json() as Promise<Product[]>;
     },
   });
 }
