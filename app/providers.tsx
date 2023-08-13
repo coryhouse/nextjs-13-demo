f43ecb51ad35cb2612641b31222585d0bedbd1b6
Cory House <housecor@gmail.com>
2023-08-13 18:29:10 -0500

    add comment

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,7 @@
 
 import { CartContextProvider } from "@/context/cart-context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// See https://tanstack.com/query/v5/docs/react/examples/react/nextjs-suspense-streaming
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 import { useState } from "react";
 
