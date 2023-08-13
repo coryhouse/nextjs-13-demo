92c2fca888a60b7938c684e51dbf741fc49711f0
Cory House <housecor@gmail.com>
2023-08-13 16:05:51 -0500

    move

---
+++ mock-data/products.ts
@@ -0,0 +1,18 @@
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
+};
+
+export const mockProducts: Product[] = [
+  {
+    id: 1,
+    name: "Nike Air Max 90",
+    price: 100,
+  },
+  {
+    id: 2,
+    name: "Puma RS-X",
+    price: 120,
+  },
+];
