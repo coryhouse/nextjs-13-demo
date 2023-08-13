27785b5c47e5eb572c9fec59b28e51644cac3c4e
Cory House <housecor@gmail.com>
2023-08-13 18:28:48 -0500

    add stale time

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,14 +3,24 @@
 import { CartContextProvider } from "@/context/cart-context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
-
-const queryClient = new QueryClient();
+import { useState } from "react";
 
 type ProvidersProps = {
   children: React.ReactNode;
 };
 
 export default function Providers({ children }: ProvidersProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 5 * 1000,
+          },
+        },
+      })
+  );
+
   return (
     <CartContextProvider>
       <QueryClientProvider client={queryClient}>
