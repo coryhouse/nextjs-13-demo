066e1e8bd63ec8881a7503f5a4aff1b0cf696436
Cory House <housecor@gmail.com>
2023-08-13 17:22:36 -0500

    show cart length in nav

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,10 +1,15 @@
+"use client";
+
+import { useCartContext } from "@/context/cart-context";
 import Link from "next/link";
 
 export default function Nav() {
+  const { cart } = useCartContext();
+
   return (
     <nav>
       <Link href="/">Home</Link> | <Link href="/products">Products</Link> |{" "}
-      <Link href="/cart">Cart</Link>
+      <Link href="/cart">Cart ({cart.length})</Link>
     </nav>
   );
 }
