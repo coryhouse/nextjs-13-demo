4d387f3f518714910ad4ad11cb5ca197a04992ef
Cory House <housecor@gmail.com>
2023-08-13 18:22:44 -0500

    add hook

---
+++ hooks/react-query/useProducts.ts
@@ -0,0 +1,12 @@
+import { Product } from "@/mock-data/products";
+import { useQuery } from "@tanstack/react-query";
+
+export function useProduct(id: string) {
+  return useQuery({
+    queryKey: ["product", id],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:3000/api/products/${id}`);
+      return res.json() as Promise<Product>;
+    },
+  });
+}
