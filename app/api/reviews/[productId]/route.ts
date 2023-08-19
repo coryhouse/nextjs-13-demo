e910e70274ba8861b5ebf06f2d95a8d216ffa1b3
Cory House <housecor@gmail.com>
2023-08-19 09:57:06 -0600

    fix resp

--- app/api/reviews/[productId]/route.ts
+++ app/api/reviews/[productId]/route.ts
@@ -13,8 +13,10 @@ export async function GET(
     (p) => p.id.toString() === params.productId
   );
 
+  // console.log("matching reviews", matchingReviews);
+
   // add a fake delay
   // setTimeout(function () {
-  return matchingReviews ? NextResponse.json(matchingReviews) : [];
+  return NextResponse.json(matchingReviews : []);
   // }, delayInMilliseconds);
 }
