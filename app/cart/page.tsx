9ca4b12465825c82607a5ce68637d4497ffec841
Cory House <housecor@gmail.com>
2023-08-18 19:39:02 -0600

    implement remove

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,14 +2,19 @@
 import { useCartContext } from "@/context/cart-context";
 
 export default function CartPage() {
-  const { cart } = useCartContext();
+  const { cart, removeFromCart } = useCartContext();
 
   return (
     <>
       <h1>Cart</h1>
       <ul>
         {cart.map((productId) => (
-          <li key={productId}>{productId}</li>
+          <li key={productId}>
+            {productId}{" "}
+            <button onClick={() => removeFromCart(productId.toString())}>
+              Remove
+            </button>
+          </li>
         ))}
       </ul>
     </>
