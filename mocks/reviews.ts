99fc28c500cd4cc88ad4c3402d44a1c0c99a9f0c
Cory House <housecor@gmail.com>
2023-08-19 10:37:53 -0600

    update

--- mocks/reviews.ts
+++ mocks/reviews.ts
@@ -9,13 +9,13 @@ export const mockReviews: Review[] = [
   {
     id: 1,
     productId: 1,
-    content: "I love it!",
+    content: "I love my Nike's!",
     author: "Pam Smith",
   },
   {
     id: 2,
     productId: 2,
-    content: "It's great!",
-    author: "Sam Lovey",
+    content: "Puma's are great!",
+    author: "Sam Lovey2",
   },
 ];
