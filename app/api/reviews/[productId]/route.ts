c76209df2f2130d68b38fce8d93dd5f43b106ced
Cory House <housecor@gmail.com>
2023-08-19 08:43:32 -0600

    fix

--- app/api/reviews/[productId]/route.ts
+++ app/api/reviews/[productId]/route.ts
@@ -9,15 +9,12 @@ export async function GET(
   request: Request,
   { params }: GetProductReviewParams
 ) {
-  if (!params.productId)
-    return NextResponse.next({ status: 400, statusText: "Missing productId." });
-
   const matchingReview = mockReviews.find(
     (p) => p.id.toString() === params.productId
   );
 
   // add a fake delay
-  setTimeout(function () {
-    return matchingReview ? NextResponse.json(matchingReview) : [];
-  }, delayInMilliseconds);
+  // setTimeout(function () {
+  return matchingReview ? NextResponse.json(matchingReview) : [];
+  // }, delayInMilliseconds);
 }
